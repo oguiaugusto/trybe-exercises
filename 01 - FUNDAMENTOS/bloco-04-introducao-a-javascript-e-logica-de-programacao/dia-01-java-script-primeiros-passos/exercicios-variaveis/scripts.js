750c3f1cec6591c08e2bf7ce30064e396806c7b7
gui <guilhaugusto05@gmail.com>
2021-08-25 16:02:01 -0300

    Adiciona exercicio de variaveis

---
+++ 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-java-script-primeiros-passos/exercicios-variaveis/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Guilherme";
+const birthCity = "São João de Meriti";
+let birthYear = 2003;
+
+var currentYear = new Date().getFullYear();
+let myAge = currentYear - birthYear;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+console.log(myAge);
