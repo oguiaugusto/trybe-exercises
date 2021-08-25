a3c9c914f356bc123e9f14319b94aec7c758bd00
gui <guilhaugusto05@gmail.com>
2021-08-25 20:34:07 -0300

    Exercicio operador ||

--- 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-java-script-primeiros-passos/exercicios-operadores-logicos/script.js
+++ 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-java-script-primeiros-passos/exercicios-operadores-logicos/script.js
@@ -8,23 +8,38 @@
 // - Se o horário estiver entre 4 e 11, insira "Hmmm, cheiro de café recém passado" na variável "message".
 // 4. Agora imprima a variável message fora das suas condições.
 
-const currentHour = 14;
-var message = ""
+// const currentHour = 14;
+// let message = ""
 
-if (currentHour >= 22 || currentHour < 4 ) {
-  message = "Não deveríamos comer nada, é hora de dormir";
-}
-else if (currentHour >= 18 && currentHour < 22) {
-  message = "Rango da noite, vamos jantar :D";
-}
-else if (currentHour >= 14 && currentHour < 18) {
-  message = "Vamos fazer um bolo pro café da tarde?";
-}
-else if (currentHour >= 11 && currentHour < 14) {
-  message = "Hora do almoço!";
+// if (currentHour >= 22 || currentHour < 4 ) {
+//   message = "Não deveríamos comer nada, é hora de dormir";
+// }
+// else if (currentHour >= 18 && currentHour < 22) {
+//   message = "Rango da noite, vamos jantar :D";
+// }
+// else if (currentHour >= 14 && currentHour < 18) {
+//   message = "Vamos fazer um bolo pro café da tarde?";
+// }
+// else if (currentHour >= 11 && currentHour < 14) {
+//   message = "Hora do almoço!";
+// }
+// else {
+//   message = "Hmmm, cheiro de café recém passado";
+// }
+
+// console.log(message)
+
+// 1. Crie uma variável chamada "weekDay" que recebe a string "quarta-feira".
+// 2. Implemente condicionais para que:
+//  - Se nossa variável "weekDay" for "segunda-feira", "terça-feira", "quarta-feira", "quinta-feira" ou "sexta-feira", imprima "Oba, mais um dia de aprendizado na Trybe >:D".
+//  - Se for algum dia de fim de semana, imprima "FINALMENTE, descando merecido UwU".
+// Experimente trocar o valor da string ou até mesmo montar seu próprio algoritmo. Esse assunto é muito importante para seu aprendizado :).
+
+let weekDay = "sábado"
+
+if (weekDay == "segunda-feira" || weekDay == "terça-feira" || weekDay == "quarta-feira" || weekDay == "quinta-feira" || weekDay == "sexta-feira") {
+  console.log("Oba, mais um dia de aprendizado na Trybe");
 }
 else {
-  message = "Hmmm, cheiro de café recém passado";
+  console.log("FINALMENTE, descanso merecido UwU.");
 }
-
-console.log(message)
