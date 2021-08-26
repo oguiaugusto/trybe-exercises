2423ddac9b4b96a1b51dc58a279bcb9808ea598e
gui <guilhaugusto05@gmail.com>
2021-08-26 01:17:13 -0300

    Exercicio 10

---
+++ 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-java-script-primeiros-passos/exercicio-10.js
@@ -0,0 +1,18 @@
+// 10. Escreva um programa que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// - Atente que, sobre o custo do produto, incide um imposto de 20%.
+// - Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+// - O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+//  - valorCustoTotal = valorCusto + impostoSobreOCusto
+//  - lucro = valorVenda - valorCustoTotal (lucro de um produto)
+
+const valorCusto = 100;
+const valorVenda = 200;
+const valorCustoTotal = valorCusto + (valorCusto * 20 / 100);
+const lucro = valorVenda - valorCustoTotal
+
+if (valorCusto >= 0 && valorVenda >= 0) {
+  console.log("Lucro de mil produtos:", lucro * 1000);
+}
+else {
+  console.log("Erro: Seus valores de entrada não podem ser menores que zero")
+}
