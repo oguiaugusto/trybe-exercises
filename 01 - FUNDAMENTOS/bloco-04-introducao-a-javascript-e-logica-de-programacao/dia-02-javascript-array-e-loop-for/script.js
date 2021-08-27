2034b0f15dd9eef81378b2d7c6674d8fe90fbd2f
gui <guilhaugusto05@gmail.com>
2021-08-26 22:30:50 -0300

    Adiciona exercicio 4

--- 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -5,8 +5,17 @@ let sum = 0;
 // 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 // 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 //  - A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
+// 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
 for (let i = 0; i < numbers.length; i += 1) {
   sum += numbers[i];
 }
-console.log(sum / numbers.length);
+let average = sum / numbers.length
+if (average > 20) {
+  console.log("Sua média é: " + average);
+  console.log("Valor maior que 20");
+}
+else {
+  console.log("Sua média é: " + average);
+  console.log("Valor menor ou igual a 20");
+}
