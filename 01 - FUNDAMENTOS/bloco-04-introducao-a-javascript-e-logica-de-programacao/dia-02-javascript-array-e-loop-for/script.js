f21962057d5aa283d8d022e8745978eaf8707a68
gui <guilhaugusto05@gmail.com>
2021-08-26 23:17:11 -0300

    Adiciona exercicio 7

--- 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -1,5 +1,5 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let oddNumbers = 0;
+let lowerNumber = numbers[0];
 
 // 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 // 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
@@ -8,15 +8,11 @@ let oddNumbers = 0;
 // 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 // 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 // 6. Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: "nenhum valor ímpar encontrado";
+// 7. Utilizando for , descubra qual o menor valor contido no array e imprima-o;
 
-for (let i = 0; i < numbers.length; i += 1) {
-  if ((numbers[i] % 2) !== 0) {
-    oddNumbers += 1;
+for (let i = 1; i < numbers.length; i += 1) {
+  if (numbers[i] < lowerNumber) {
+    lowerNumber = numbers[i];
   }
 }
-if (oddNumbers > 0) {
-  console.log(oddNumbers);
-}
-else {
-  console.log("Nenhum valor ímpar encontrado")
-}
+console.log(lowerNumber);
