d667e29854ebd21718100d04f7e1551e040bc7fa
gui <guilhaugusto05@gmail.com>
2021-08-26 22:45:37 -0300

    Adiciona exercicio 5

--- 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
+++ 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/script.js
@@ -1,21 +1,18 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let sum = 0;
+let higherNumber = 0;
 
 // 1. Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log();
 // 2. Para o segundo exercício, some todos os valores contidos no array e imprima o resultado;
 // 3. Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 //  - A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 // 4. Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+// 5. Utilizando for , descubra qual o maior valor contido no array e imprima-o;
 
 for (let i = 0; i < numbers.length; i += 1) {
-  sum += numbers[i];
+  if (numbers[i] > higherNumber) {
+    higherNumber = numbers[i];
+  }
+  else {
+  }
 }
-let average = sum / numbers.length
-if (average > 20) {
-  console.log("Sua média é: " + average);
-  console.log("Valor maior que 20");
-}
-else {
-  console.log("Sua média é: " + average);
-  console.log("Valor menor ou igual a 20");
-}
+console.log(higherNumber)
