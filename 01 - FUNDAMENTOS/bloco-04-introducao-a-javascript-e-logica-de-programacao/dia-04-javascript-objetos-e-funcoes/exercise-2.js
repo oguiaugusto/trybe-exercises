ab8e7d4c711648e5fe239a15501735f434146795
gui <guilhaugusto05@gmail.com>
2021-08-30 20:33:23 -0300

    Adiciona item 2 do exercicio 2

--- 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercise-2.js
+++ 01 - FUNDAMENTOS/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-04-javascript-objetos-e-funcoes/exercise-2.js
@@ -16,3 +16,24 @@ function verifyPalindrome(word) {
   }
 }
 console.log(verifyPalindrome('arara'));
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+function biggersIndex(array) {
+  if (Array.isArray(array) === true && array.length === 0) {
+    return "Você precisa inserir algum número no array."
+  }
+  else if (Array.isArray(array) === true) {
+    let biggerNumber = array[0];
+    for (let i in array) {
+      let currentNumber = array[i];
+      if (currentNumber > biggerNumber) {
+        biggerNumber = currentNumber;
+      }
+    }
+    return array.indexOf(biggerNumber);
+  }
+  else {
+    return "Você precisa inserir um array."
+  }
+}
+console.log(biggersIndex([2, 3, 6, 7, 10, 1]))
