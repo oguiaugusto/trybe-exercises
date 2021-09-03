649e3ea1af691f2cc3022dced43464002c7d4eac
gui <guilhaugusto05@gmail.com>
2021-09-03 17:19:40 -0300

    Tarefa 1

--- 01 - FUNDAMENTOS/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script-exercicios.js
+++ 01 - FUNDAMENTOS/bloco-05-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script-exercicios.js
@@ -1,5 +1,8 @@
 // 1. Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body;
-
+let body = document.querySelector('body');
+let h1 = document.createElement('h1');
+h1.innerText = 'Exercício 5.2 - JavaScript DOM';
+body.appendChild(h1);
 
 // 2. Adicione a tag main com a classe main-content como filho da tag body;
 
