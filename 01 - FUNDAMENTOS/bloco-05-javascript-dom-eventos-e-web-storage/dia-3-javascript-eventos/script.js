25dc92fb6f7f6b601d24a3187e3c2cd2127f63fb
gui <guilhaugusto05@gmail.com>
2021-09-08 16:18:21 -0300

    Exercicio 1

--- 01 - FUNDAMENTOS/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ 01 - FUNDAMENTOS/bloco-05-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -14,3 +14,21 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+function createCalendarDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    let currentDay = document.createElement('li');
+    currentDay.className = 'day';
+    currentDay.innerText = dezDaysList[i];
+    if (dezDaysList[i] === 24 || dezDaysList[i] === 25 || dezDaysList[i] === 31) {
+      currentDay.classList.add('holiday');
+    }
+    if (dezDaysList[i] === 4 || dezDaysList[i] === 11 || dezDaysList[i] === 18 || dezDaysList[i] === 25) {
+      currentDay.classList.add('friday');
+    }
+    days.appendChild(currentDay);
+  }
+}
+
+createCalendarDays();
