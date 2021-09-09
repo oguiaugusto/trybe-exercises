d2ac153fe53080c05d784d26194ce13f2e7c2b54
gui <guilhaugusto05@gmail.com>
2021-09-09 18:43:36 -0300

    Adiciona const dos inputs

---
+++ 01 - FUNDAMENTOS/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -0,0 +1,5 @@
+const inputBgColor = document.getElementById('bg-color-input');
+const inputTextColor = document.getElementById('text-color-input');
+const inputFontSize = document.getElementById('font-size-input');
+const inputLineHeight = document.getElementById('line-height-input');
+const inputFontType = document.getElementById('font-type-input');
