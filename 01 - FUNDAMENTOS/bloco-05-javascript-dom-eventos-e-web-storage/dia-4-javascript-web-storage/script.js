56a052ecf3530e9a23d6052309e7adac07788542
gui <guilhaugusto05@gmail.com>
2021-09-09 19:00:15 -0300

    Funçao que troca a cor de fundo

--- 01 - FUNDAMENTOS/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
+++ 01 - FUNDAMENTOS/bloco-05-javascript-dom-eventos-e-web-storage/dia-4-javascript-web-storage/script.js
@@ -3,3 +3,21 @@ const inputTextColor = document.getElementById('text-color-input');
 const inputFontSize = document.getElementById('font-size-input');
 const inputLineHeight = document.getElementById('line-height-input');
 const inputFontType = document.getElementById('font-type-input');
+
+const buttonBgColor = document.getElementById('bg-color-button');
+
+const body = document.querySelector('body');
+
+function changeBgColor() {
+  let color = inputBgColor.value;
+  body.style.backgroundColor = `${color}`;
+  inputBgColor.value = '';
+}
+
+buttonBgColor.addEventListener('click', changeBgColor);
+inputBgColor.addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    changeBgColor();
+  }
+});
