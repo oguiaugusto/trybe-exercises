1aa74f53be15d036109aa6935c22f6903c0b9b42
gui <guilhaugusto05@gmail.com>
2021-09-24 15:47:08 -0300

    Fixaçao 2

--- 01 - FUNDAMENTOS/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/fixacao.js
+++ 01 - FUNDAMENTOS/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-objects/fixacao.js
@@ -1,13 +1,42 @@
 // Agora, para praticar, crie uma função que receba três parâmetros, sendo eles: um objeto, o nome de uma chave e o seu valor. O retorno dessa função deve ser o objeto já com a nova chave adicionada nele.
-const fruit = {
-  fruta: 'banana',
+// const fruit = {
+//   fruta: 'banana',
+// };
+
+// const setProperty = (object, key, value) => {
+//   const newKey = `${key}`;
+//   const newValue = `${value}`;
+//   object[newKey] = newValue;
+//   return object;
+// };
+
+// console.log(setProperty(fruit, 'cor', 'amarela'));
+
+
+// Tente criar uma função que exiba as habilidades do objeto student . Cada habilidade deve ser exibida no formato "Nome da habilidade, Nível: valor da chave referente à habilidade". Após tentar criar a função, veja a sugestão de resposta logo abaixo e compare com a sua solução.
+
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
 };
 
-const setProperty = (object, key, value) => {
-  const newKey = `${key}`;
-  const newValue = `${value}`;
-  object[newKey] = newValue;
-  return object;
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
 };
 
-console.log(setProperty(fruit, 'cor', 'amarela'));
+const listSkills = (object) => {
+  const skills = Object.keys(object);
+  for (let i = 0; i < skills.length; i += 1) {
+    console.log(`${skills[i]}, Nível: ${object[skills[i]]}`);
+  }
+};
+
+listSkills(student1);
+console.log('---');
+listSkills(student1);
