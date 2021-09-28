b05200f58e4258e8abb2b23f03e4583f3620cfcd
gui <guilhaugusto05@gmail.com>
2021-09-28 20:22:34 -0300

    exercicio 2

--- 01 - FUNDAMENTOS/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
+++ 01 - FUNDAMENTOS/bloco-07-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/script.test.js
@@ -1,6 +1,9 @@
-const sum = require('./sum');
+const {
+  sum,
+  myRemove,
+} = require('./script');
 
-describe('A função sum()', () => {
+describe('Função sum()', () => {
   it('sum(4, 5) é igual a 9', () => {
     expect(sum(4, 5)).toEqual(9);
   });
@@ -12,5 +15,20 @@ describe('A função sum()', () => {
   });
   it('a mensagem de erro "parameters must be numbers" é lançada ao realizar sum(4, "5")', () => {
     expect(() => {sum(4, '5')}).toThrow('parameters must be numbers');
+  });
+});
+
+describe('Função myRemove()', () => {
+  it('myRemove([1, 2, 3, 4], 3) retorna [1, 2, 4]', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 3)).toEqual([1, 2, 4]);
+  });
+  it('myRemove([1, 2, 3, 4], 3) NÃO retorna [1, 2, 3, 4]', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 3)).not.toEqual(array);
+  });
+  it('myRemove([1, 2, 3, 4], 5) retorna [1, 2, 3, 4]', () => {
+    const array = [1, 2, 3, 4];
+    expect(myRemove(array, 5)).toEqual(array);
   });
 });
