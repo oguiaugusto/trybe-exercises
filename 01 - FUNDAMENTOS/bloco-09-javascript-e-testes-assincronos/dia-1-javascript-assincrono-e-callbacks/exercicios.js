deef23da7aa342a78e407ef7f4110ff391c882cd
Guilherme <guilhaugusto05@gmail.com>
2021-10-15 21:20:25 -0300

    Exercício 5

--- 01 - FUNDAMENTOS/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios.js
+++ 01 - FUNDAMENTOS/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios.js
@@ -59,4 +59,39 @@ const uppercase = (str, callback) => {
 
 // const printMessage = (message) => console.log(message);
 
-module.exports = { uppercase };
+// ------
+
+const pokemons = [
+  {
+    name: 'Bulbasaur',
+    type: 'Grass',
+    ability: 'Razor Leaf',
+  },
+  {
+    name: 'Charmander',
+    type: 'Fire',
+    ability: 'Ember',
+  },
+  {
+    name: 'Squirtle',
+    type: 'Water',
+    ability: 'Water Gun',
+  },
+];
+
+function getPokemonDetails(filter, callback) {
+  setTimeout(() => {
+    if (pokemons.find(filter) === undefined) {
+      return callback(new Error('Não temos esse pokémon para você :('), null);
+    }
+    const pokemon = pokemons.find(filter);
+
+    const { name, type, ability } = pokemon;
+
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+
+    callback(null, messageFromProfOak);
+  }, 2000);
+}
+
+module.exports = { uppercase, getPokemonDetails };
--- 01 - FUNDAMENTOS/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios.test.js
+++ 01 - FUNDAMENTOS/bloco-09-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercicios.test.js
@@ -1,4 +1,4 @@
-const { uppercase } = require('./exercicios')
+const { uppercase, getPokemonDetails } = require('./exercicios')
 
 describe('Testa se a função uppercase() funciona como esperado', () => {
   it(`Testa se é imprimida a palavra 'HELLO' ao passar para a função a palavra 'hello' `, (done) => {
@@ -13,4 +13,31 @@ describe('Testa se a função uppercase() funciona como esperado', () => {
       }
     });
   });
-})
+});
+
+describe('Testa se a função getPokemonDetails() funciona como esperado', () => {
+  it('Testa se, ao passar um pokemon que não está contido no objeto de pokemons, ela lança um erro', (done) => {
+    const expected = new Error('Não temos esse pokémon para você :(');
+    const callback = (error, result) => {
+      try {
+        expect(error).toEqual(expected);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    };
+    getPokemonDetails((pokemon) => pokemon.name === 'Pidgeot', callback);
+  });
+  it('Testa se, ao passar Charmander para a função, ela retorna a string correta', (done) => {
+    const expected = 'Olá, seu pokémon é o Charmander, o tipo dele é Fire e a habilidade principal dele é Ember';
+    const callback = (error, result) => {
+      try {
+        expect(result).toEqual(expected);
+        done();
+      } catch (error) {
+        done(error);
+      }
+    };
+    getPokemonDetails((pokemon) => pokemon.name === 'Charmander', callback);
+  });
+});
