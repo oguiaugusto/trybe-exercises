3f02208abc5615fd8f53deb06cbecbf8eaa63a02
Guilherme <guilhaugusto05@gmail.com>
2021-10-18 15:32:54 -0300

    Fixação fetch api

---
+++ 01 - FUNDAMENTOS/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-async-await/fixacao-1/fetch.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value));
+  }
+
+const fetchJokeError = () => {
+  const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errrado :( \n${error}
+      `));
+}
+
+fetchJoke();
+fetchJokeError();
