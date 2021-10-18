67c216bebf212ae1a92fee6ee9c3040df1cc643e
Guilherme <guilhaugusto05@gmail.com>
2021-10-18 16:07:28 -0300

    Fixação async e await

---
+++ 01 - FUNDAMENTOS/bloco-09-javascript-e-testes-assincronos/dia-2-javascript-assincrono-fetch-api-e-async-await/fixacao-1/async-await.js
@@ -0,0 +1,18 @@
+const fetch = require('node-fetch');
+
+const fetchJoke = async () => {
+  const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.value);
+  } catch (error) {
+    console.log(`Algo deu errado :( 
+${error}`);
+  }
+}
+
+fetchJoke();
+
+// Chuck Norris can write multi-threaded applications with a single thread.
