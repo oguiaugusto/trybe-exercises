15b7b5a6e5bfa943bc52659c6ddfce683b2df12b
Guilherme <guilhaugusto05@gmail.com>
2021-10-27 19:37:30 -0300

    Exercício 1 (1 e 2 no course)

--- 02 - FRONT-END/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-1/src/App.js
+++ 02 - FRONT-END/bloco-10-introducao-a-react/dia-1-hello-world-no-react/exercicio-1/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './App.css';
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+};
+
+const tasks = ['Terminar os exercícios', 'Comer algo', 'Estudar mais um pouco', 'Dormir']
+
 function App() {
-  return ();
+  return (
+    <ol>
+      {tasks.map((currTask) => Task(currTask))}
+    </ol>
+  );
 }
 
 export default App;
