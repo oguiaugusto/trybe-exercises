b2f5d2d2fbcb02cc3cb2fb86eff76061cf032eaa
Guilherme <guilhaugusto05@gmail.com>
2021-10-27 19:12:17 -0300

    Fixação 1

--- 02 - FRONT-END/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixacao/src/App.js
+++ 02 - FRONT-END/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixacao/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import Component from './Component';
 import './App.css';
 
 function App() {
-  return ();
+  return (
+    <Component />
+  );
 }
 
 export default App;
---
+++ 02 - FRONT-END/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixacao/src/Component.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Component extends React.Component {
+  render() {
+    return (
+      <section className='about-me'>
+        <h1>Guilherme Augusto</h1>
+        <p>Estudante de Desenvolvimento de Software</p>
+      </section>
+    )
+  }
+}
+
+export default Component;
