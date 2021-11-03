5c326f5fe949b89c3dd18db01e9650a52962190f
Guilherme <guilhaugusto05@gmail.com>
2021-11-03 14:34:02 -0300

    Fixação 1.2

--- 02 - FRONT-END/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02 - FRONT-END/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -3,10 +3,22 @@ import './App.css';
 
 class App extends React.Component {
   render() {
-    function handleClick() {
-      console.log('Hello, world!');
+    function handleClick1() {
+      console.log('Texto 1');
     }
-    return <button onClick={handleClick}>Click-me</button>;
+    function handleClick2() {
+      console.log('TEXTO 2');
+    }
+    function handleClick3() {
+      console.log('texto 3');
+    }
+    return (
+      <>
+        <button onClick={handleClick1}>Click-me</button>
+        <button onClick={handleClick2}>Click-me</button>
+        <button onClick={handleClick3}>Click-me</button>
+      </>
+    );
   }
 }
  
