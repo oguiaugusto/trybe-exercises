3c35c71f56ade3521e0c05a6db26d2ad3813c7ce
Guilherme <guilhaugusto05@gmail.com>
2021-11-03 15:29:01 -0300

    Fixação 1.5, 6 e 7

--- 02 - FRONT-END/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ 02 - FRONT-END/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -3,31 +3,50 @@ import './App.css';
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
+
+    this.state = {
+      button1: 0,
+      button2: 0,
+      button3: 0,
+    }
+
     this.handleClick1 = this.handleClick1.bind(this);
     this.handleClick2 = this.handleClick2.bind(this);
     this.handleClick3 = this.handleClick3.bind(this);
   }
 
   handleClick1() {
-    console.log(this);
-    console.log('Texto 1');
+    this.setState((prevState, _props) => ({
+      button1: prevState.button1 + 1,
+    }));
   }
   handleClick2() {
-    console.log(this);
-    console.log('TEXTO 2');
+    this.setState((prevState, _props) => ({
+      button2: prevState.button2 + 1,
+    }));
   }
   handleClick3() {
-    console.log(this);
-    console.log('texto 3');
+    this.setState((prevState, _props) => ({
+      button3: prevState.button3 + 1,
+    }));
   }
 
   render() {
     return (
       <>
-        <button onClick={this.handleClick1}>Click-me</button>
-        <button onClick={this.handleClick2}>Click-me</button>
-        <button onClick={this.handleClick3}>Click-me</button>
+        <div>
+          <button onClick={this.handleClick1}>Click-me</button>
+          <p>{this.state.button1}</p>
+        </div>
+        <div>
+          <button onClick={this.handleClick2}>Click-me</button>
+          <p>{this.state.button2}</p>
+        </div>
+        <div>
+          <button onClick={this.handleClick3}>Click-me</button>
+          <p>{this.state.button3}</p>
+        </div>
       </>
     );
   }
