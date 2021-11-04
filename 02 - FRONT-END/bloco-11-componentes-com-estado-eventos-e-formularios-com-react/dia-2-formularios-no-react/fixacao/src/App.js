b2ee99e1231af91982c2fa242bad7b3cf69d9a4c
Guilherme <guilhaugusto05@gmail.com>
2021-11-04 15:18:59 -0300

    Fixação 1.5 e 6

--- 02 - FRONT-END/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/src/App.js
+++ 02 - FRONT-END/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-2-formularios-no-react/fixacao/src/App.js
@@ -28,31 +28,44 @@ class App extends React.Component {
       <>
         <h1>Formulário qualquer</h1>
         <form>
-          <div>
-            <label for="nameInput">Insira seu nome</label>
-            <input type="text" id="nameInput" name="nameInput" value={this.state.name} onChange={this.handleChange} />
-          </div>
-          <div>
-            <label for="age">Insira sua idade</label>
-            <input type="number" id="age" name="age" value={this.state.age} onChange={this.handleChange} />
-          </div>
-          <div>
-            <label for="fruits">Selecione uma fruta</label>
-            <select name="fruits" id="fruits" value={this.state.fruit} onChange={this.handleChange}>
-              <option>Banana</option>
-              <option>Maçã</option>
-              <option>Uva</option>
-              <option>Laranja</option>
-            </select>
-          </div>
-          <div>
-            <label for="comment">Diga algo</label>
-            <textarea id="comment" name="comment" rows="4" cols="25" value={this.state.comment} onChange={this.handleChange} ></textarea>
-          </div>
-          <div>
-            <label for="everythingOk">Tudo certo?</label>
-            <input type="checkbox" id="everythingOk" name="everythingOk" checked={this.state.everythingOk} onChange={this.handleChange}></input>
-          </div>
+          <fieldset>
+            <legend>Sobre Você</legend>
+            <div>
+              <label for="nameInput">Insira seu nome</label>
+              <input type="text" id="nameInput" name="nameInput" value={this.state.name} onChange={this.handleChange} />
+            </div>
+            <div>
+              <label for="age">Insira sua idade</label>
+              <input type="number" id="age" name="age" value={this.state.age} onChange={this.handleChange} />
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>Algumas preferências</legend>
+            <div>
+              <label for="fruits">Selecione uma fruta</label>
+              <select name="fruits" id="fruits" value={this.state.fruit} onChange={this.handleChange}>
+                <option>Banana</option>
+                <option>Maçã</option>
+                <option>Uva</option>
+                <option>Laranja</option>
+              </select>
+            </div>
+            <div>
+              <label for="comment">Diga algo</label>
+              <textarea id="comment" name="comment" rows="4" cols="25" value={this.state.comment} onChange={this.handleChange} ></textarea>
+            </div>
+          </fieldset>
+          <fieldset>
+            <legend>Algo a adicionar?</legend>
+            <div>
+              <label for="everythingOk">Tudo certo?</label>
+              <input type="checkbox" id="everythingOk" name="everythingOk" checked={this.state.everythingOk} onChange={this.handleChange}></input>
+            </div>
+            <div>
+              <label for="shareFile">Algum arquivo que queira compartilhar?</label>
+              <input type="file" id="shareFile" name="shareFile"></input>
+            </div>
+          </fieldset>
         </form>
       </>
     );
