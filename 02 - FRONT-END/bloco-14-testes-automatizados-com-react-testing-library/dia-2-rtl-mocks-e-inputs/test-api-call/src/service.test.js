b8b0e12f0554b0e589b8ee9e829f0f8ae7088e49
Guilherme <guilhaugusto05@gmail.com>
2021-11-26 17:30:01 -0300

    Exercício 1.2

--- 02 - FRONT-END/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/test-api-call/src/service.test.js
+++ 02 - FRONT-END/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/test-api-call/src/service.test.js
@@ -11,4 +11,11 @@ describe('Testa funções do arquivo service.js', () => {
     expect(randomNumber).toHaveBeenCalledTimes(1);
     expect(randomNumber()).toBe(10);
   });
+  it('Testa se, ao trocar a implementação, randomNumber retorna a divisão esperada', () => {
+    randomNumber.mockImplementation((n1, n2) => n1 / n2);
+
+    randomNumber(6, 2);
+    expect(randomNumber).toHaveBeenCalledTimes(1);
+    expect(randomNumber(6, 2)).toBe(3);
+  });
 });
