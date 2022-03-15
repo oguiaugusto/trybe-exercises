89a9f379b18bb1b9643773e8e1ec4fd841960235
Guilherme <guilhaugusto05@gmail.com>
2022-03-15 19:23:21 -0300

    Exercício 2

--- 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-node-js/exercicio/checarNumero.js
+++ 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-3-mocha-chai-e-sinon-testes-de-back-end-com-node-js/exercicio/checarNumero.js
@@ -1,3 +1,7 @@
-const checarNumero = (n) => {};
+const checarNumero = (n) => {
+  if (n < 0) return 'negativo';
+  if (n > 0) return 'positivo';
+  return 'neutro';
+};
 
 module.exports = checarNumero;
