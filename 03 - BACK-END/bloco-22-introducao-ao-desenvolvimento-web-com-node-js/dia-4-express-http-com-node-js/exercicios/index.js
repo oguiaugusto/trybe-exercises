8678f2e2f5f2dd8eca6f81c69475237b8edc8701
Guilherme <guilhaugusto05@gmail.com>
2022-03-16 21:24:10 -0300

    Exercício 2

--- 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-node-js/exercicios/index.js
+++ 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-node-js/exercicios/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/ping', (_req, res) => res.json({ message: 'pong' }));
+app.post('/hello', (req, res) => res.json({ message: `Hello, ${req.body.name}` }));
 
 app.listen(3001, () => console.log('Running on port 3001'));
