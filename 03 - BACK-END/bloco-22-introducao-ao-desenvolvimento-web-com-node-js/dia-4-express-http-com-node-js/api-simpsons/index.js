c50c6381451feba27faec47843a8b02064e607fa
Guilherme <guilhaugusto05@gmail.com>
2022-03-16 21:47:35 -0300

    Exercício 6

--- 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-node-js/api-simpsons/index.js
+++ 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-node-js/api-simpsons/index.js
@@ -5,4 +5,9 @@ const readSimpsons = require('./readSimpsons');
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/simpsons', async (req, res) => {
+  const simpsons = await readSimpsons();
+  res.status(200).json(simpsons);
+});
+
 app.listen(3001, () => console.log('Running on port 3001'));
