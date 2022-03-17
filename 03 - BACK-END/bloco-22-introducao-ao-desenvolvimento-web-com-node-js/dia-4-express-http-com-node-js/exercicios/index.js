4b12abe6d5edb4bebe5f7edb1bc0d2d21e072772
Guilherme <guilhaugusto05@gmail.com>
2022-03-16 21:36:41 -0300

    Exercício 4

--- 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-node-js/exercicios/index.js
+++ 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-4-express-http-com-node-js/exercicios/index.js
@@ -12,5 +12,10 @@ app.post('/greetings', (req, res) => {
   if (parseInt(age) > 17) return res.status(200).json({ message: `Hello, ${name}` });
   return res.status(401).json({ message: 'Unauthorized' });
 });
+app.put('/users/:name/:age', (req, res) => {
+  const { name, age } = req.params;
+
+  res.json({ message: `Seu nome é ${name} e você tem ${age} anos de idade` });
+});
 
 app.listen(3001, () => console.log('Running on port 3001'));
