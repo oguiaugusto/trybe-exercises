0eea2bb33a47ac7d75e35d10af509943884ee37d
Guilherme <guilhaugusto05@gmail.com>
2022-03-17 22:02:18 -0300

    Atividade 3.6

--- 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/atividade-1/index.js
+++ 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/atividade-1/index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const btcRoutes = require('./routes/btcRoutes');
 const postsRoutes = require('./routes/postsRoutes');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,5 +11,14 @@ app.use(bodyParser.json());
 app.use('/user', userRoutes);
 app.use('/btc', btcRoutes);
 app.use('/posts', postsRoutes);
+
+app.all('*', (_req, _res, next) => {
+  const error = new Error;
+  error.message = 'Opss, route not found!';
+  error.status = 404;
+  return next(error);
+});
+
+app.use(errorMiddleware);
 
 app.listen(3001, () => console.log('Running on port 3001'));
