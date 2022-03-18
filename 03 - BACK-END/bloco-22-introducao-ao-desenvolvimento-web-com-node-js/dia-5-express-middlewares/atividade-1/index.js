7086cdb4f847f999f7ff2a08fa51420dda7dcec8
Guilherme <guilhaugusto05@gmail.com>
2022-03-17 21:50:11 -0300

    Atividade 3.4

--- 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/atividade-1/index.js
+++ 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/atividade-1/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
-const btcRoutes = require('./routes/btcRoutes')
+const btcRoutes = require('./routes/btcRoutes');
+const postsRoutes = require('./routes/postsRoutes');
 
 const app = express();
 app.use(bodyParser.json());
 
 app.use('/user', userRoutes);
 app.use('/btc', btcRoutes);
+app.use('/posts', postsRoutes);
 
 app.listen(3001, () => console.log('Running on port 3001'));
---
+++ 03 - BACK-END/bloco-22-introducao-ao-desenvolvimento-web-com-node-js/dia-5-express-middlewares/atividade-1/routes/postsRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const posts = require('../data/postsData');
+
+const router = express.Router();
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  const post = posts.find((p) => p.id === parseInt(id));
+
+  if (!id || !post) return res.status(404).json({ status: 404, message: 'post not found' });
+  return res.status(200).json(post);
+});
+
+module.exports = router;
